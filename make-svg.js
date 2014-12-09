44f392fa885a8f7b180c05cf942cc5fb103aae73
An Ko <an@cyan.io>
2014-12-09 01:34:53 +0100

    D3-render chart

--- make-svg.js
+++ make-svg.js
@@ -7,6 +7,10 @@ var fs = require("fs");
 var data = (function() {
     return fs.readFileSync("data", {encoding:"utf-8"})
         .split("\n")
+        .filter(function(line) {
+            // Ignore empty lines
+            return line.length > 0;
+        })
         .map(function(line) {
             var fields = line.split(" ");
             return {
@@ -16,20 +20,44 @@ var data = (function() {
         });
 })();
 
-// Create SVG element
-var svg = d3.select("body").append("svg")
-    .attr({
-        width : 500,
-        height : 200
-    });
+// ---------------------
+// SVG constructor stuff
+// ---------------------
 
+var width  = 500;
+var height = 200;
+var marginLeft = 40;
+var marginTop  = 40;
+
+var x = d3.time.scale()
+    .range([0, width])
+    .domain(d3.extent(data, function(d) { return d.date; }));
+var y = d3.scale.linear()
+    .range([height, 0])
+    .domain(d3.extent(data, function(d) { return d.number; }));
+
+var xAxis = d3.svg.axis().scale(x).orient("bottom");
+var yAxis = d3.svg.axis().scale(y).orient("left");
+
+var line = d3.svg.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.number); });
+
+// -----------------------
+// Actually make the chart
+// -----------------------
+
+// Create SVG element
+var svg = d3.select("body").append("svg");
+var vis = svg.append("g")
+    .attr({ width : width, height : height })
+    .append("g")
+    .attr("transform", "translate(" + marginLeft + "," + marginTop + ")");
 
 // Make a chart
-svg.append("circle").attr({
-    cx : 50,
-    cy : 50,
-    r  : 10
-});
+svg.append("path").datum(data).attr("d", line);
+svg.append("g").call(xAxis);
+svg.append("g").call(yAxis);
 
 
 // Log the resulting SVG
