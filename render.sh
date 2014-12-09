63bc366a95c19154371467a1f064581712928d65
An Ko <an@cyan.io>
2014-12-09 01:58:30 +0100

    Plainer inkscape convert
    There's no need for the size parameters when the SVG defines them
    correctly.

--- render.sh
+++ render.sh
@@ -4,4 +4,4 @@ PNG_NAME="out.png"
 cat svg-doctype.xml > "$SVG_NAME"      # DOCTYPE declaration
 ./render-svg-content.js >> "$SVG_NAME" # Actual SVG contents
 
-inkscape -z -e "$PNG_NAME" -w 500 -h 200 "$SVG_NAME"
+inkscape -z -e "$PNG_NAME" "$SVG_NAME"
